d27d4dcd588cb9cf6f6a48fc4254f492fa009393
julijanjug <julijan.jug@gmail.com>
2018-12-09 13:53:47 +0100

    Added some login and signup toasts

--- PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/LoginActivity.java
+++ PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/LoginActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private boolean loggingIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                login();
             }
         });
 
@@ -39,5 +43,39 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, FaqActivity.class));
             }
         });
+    }
+
+    private void login() {
+        if (loggingIn){
+            return;
+        }
+        loggingIn = true;
+
+        EditText email = (EditText) findViewById(R.id.input_email);
+        EditText password = (EditText) findViewById(R.id.input_password);
+
+        if (email.getText().toString().isEmpty()) {
+            Toast.makeText(this, "You must enter your email.", Toast.LENGTH_SHORT).show();
+            loggingIn = false;
+            return;
+        }
+
+        if (password.getText().toString().isEmpty()) {
+            Toast.makeText(this, "You must enter your password.", Toast.LENGTH_SHORT).show();
+            loggingIn = false;
+            return;
+        }
+
+        //TODO: pregled podatkov v bazi
+//        API.login(email.toString(), password.toString()) { response ->
+//            if (response.success && response is API.LoginResponse) {
+//                Toast.makeText(this, "Login successful.", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "Login unsuccessful (${response.message ?: "/"})", Toast.LENGTH_SHORT).show()
+//            }
+//            loggingIn = false
+//        }
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        loggingIn = false;
     }
 }
--- PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/SignupActivity.java
+++ PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/SignupActivity.java
@@ -1,12 +1,19 @@
 package com.example.julijanjug.pocketbank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private boolean signingup = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,14 @@ public class SignupActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        Button btnSignup = (Button)findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
     }
 
     @Override
@@ -30,7 +45,40 @@ public class SignupActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home) {
             finish(); // close this activity and return to preview activity (if there is any)
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private void signup() {
+        if (signingup){
+            return;
+        }
+        signingup = true;
+
+        EditText email = (EditText) findViewById(R.id.input_email1);
+        EditText password1 = (EditText) findViewById(R.id.input_password1);
+        EditText password2 = (EditText) findViewById(R.id.input_password2);
+
+        if (email.getText().toString().isEmpty()) {
+            Toast.makeText(this, "You must enter your email.", Toast.LENGTH_SHORT).show();
+            signingup = false;
+            return;
+        }
+
+        if (password1.getText().toString().isEmpty() || password2.getText().toString().isEmpty()) {
+            Toast.makeText(this, "You must enter your password.", Toast.LENGTH_SHORT).show();
+            signingup = false;
+            return;
+        }
+
+        if (!password1.getText().toString().equals(password2.getText().toString())) {
+            Toast.makeText(this, "Passwords don't match.", Toast.LENGTH_SHORT).show();
+            signingup = false;
+            return;
+        }
+
+        //TODO: vnos podatkov v bazo
+
+        startActivity(new Intent(SignupActivity.this, MainActivity.class));
+        signingup = false;
     }
 }
