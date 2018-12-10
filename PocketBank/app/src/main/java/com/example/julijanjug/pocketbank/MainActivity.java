ddf7448348ecf51a0cfc85a72cc705d5341cbf3f
julijanjug <julijan.jug@gmail.com>
2018-12-10 21:49:53 +0100

    Added graph to main activity

--- PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/MainActivity.java
+++ PocketBank/app/src/main/java/com/example/julijanjug/pocketbank/MainActivity.java
@@ -9,6 +9,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -29,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddTransactionActivity.class));
             }
         });
+
+        //Graf
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
     }
 
     // Menu icons are inflated just as they were with actionbar
